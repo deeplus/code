757edec761c9ea5b350f10560e4be639e8b16402
deeplus <deeplus@163.com>
2020-06-10 15:35:28 +0800

    add 简约、清爽登陆页--> login2

---
+++ login/login2/index.js
@@ -0,0 +1,27 @@
+// 封装一下该功能模块
+((window, document) => {
+
+    // 封装一个类似jQuery的选择器
+    const getSelector = ele => {
+        return typeof ele === 'string' ? document.querySelector(ele): ""
+    }
+
+    // 封装一个类似jQuery的添加类方法
+    const addClass = (ele, cls) => {
+        ele.classList.add(cls)
+    }
+
+    // 封装一个类似jQuery的删除类方法
+    const removeClass = (ele, cls) => {
+        ele.classList.remove(cls)
+    }
+
+    // 获取 input和label 的Dom对象
+    let input = getSelector("input"),
+        label = getSelector("label")
+
+    input.onfocus = addClass.bind(this, label, 'animation_one')
+    input.onblur = function () {
+        if (input.value === '') removeClass(label, 'animation_one')
+    }
+})(window, document)
